476bdfeffd30d5e2861d4ed7a8f01ac0f9e0ea5b
FFFFFf4-rgp <thesoulshifter57@gmail.com>
2025-07-30 02:52:56 -0300

    README.lua

---
+++ README.lua
@@ -0,0 +1,9 @@
+local player = game.Players.LocalPlayer
+local Valor = 0  -- começa em 0
+
+-- exemplo: você muda manualmente pra 1
+Valor = 1
+
+if Valor == 1 then
+    player:Kick("Você foi kickado porque Valor = 1")
+end
